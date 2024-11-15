374fa874f30a1fcf68107a5e7adb56fdcb243202
o biano <fabinnho.10sul@gmail.com>
2024-11-15 15:58:48 -0300

    Add files via upload

---
+++ !!script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
+
+ function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('open');
+}
